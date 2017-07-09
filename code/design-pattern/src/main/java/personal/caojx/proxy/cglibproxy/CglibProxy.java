9443b742d1a8e5b3dfa4aa6bd0df84c13a3d39c1
389715062@qq.com <Github389715062>
2017-07-09 20:10:25 +0800

    设计模式更新

---
+++ code/design-pattern/src/main/java/personal/caojx/proxy/cglibproxy/CglibProxy.java
@@ -0,0 +1,13 @@
+package personal.caojx.proxy.cglibproxy;
+
+/**
+ * Copyright: Copyright (c) 2017 Asiainfo
+ *
+ * @ClassName: CglibProxy
+ * @Description: 该类的功能描述
+ * @version: v1.0.0
+ * @author: caojx
+ * @date: 17-7-9 下午7:30
+ */
+public class CglibProxy {
+}
