f8fcb62652ad1ba9ac157db2876aaed39b6f44f3
caojx <389715062@qq.com>
2018-04-30 21:34:12 +0800

    redis与spring整合，单台/多台主从/多台集群
    Former-commit-id: 042e3d1333ee7e2c9584e70ee03a571a3be7a36b

---
+++ code/spring-redis/src/test/java/SpringRedisTest2.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * spring整合单台多台redis主从
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:redis-ms-context.xml"})
+public class SpringRedisTest2 {
+    
+    @Autowired
+    private RedisTemplate redisTemplate;
+    
+    /**
+     * stringRedisTemplate的操作
+     */
+    @Test
+    public void testSpringRedis() {
+      
+        
+        // String读写
+        redisTemplate.delete("myStr");
+        redisTemplate.opsForValue().set("myStr", "skyLine");
+        System.out.println(redisTemplate.opsForValue().get("myStr"));
+        System.out.println("---------------");
+    
+        // List读写
+        redisTemplate.delete("myList");
+        redisTemplate.opsForList().rightPush("myList", "T");
+        redisTemplate.opsForList().rightPush("myList", "L");
+        redisTemplate.opsForList().leftPush("myList", "A");
+        List<String> listCache = redisTemplate.opsForList().range("myList", 0, -1);
+        for (String s : listCache) {
+            System.out.println(s);
+        }
+        System.out.println("---------------");
+    
+        // Set读写
+        redisTemplate.delete("mySet");
+        redisTemplate.opsForSet().add("mySet", "A");
+        redisTemplate.opsForSet().add("mySet", "B");
+        redisTemplate.opsForSet().add("mySet", "C");
+        Set<String> setCache = redisTemplate.opsForSet().members("mySet");
+        for (String s : setCache) {
+            System.out.println(s);
+        }
+        System.out.println("---------------");
+    
+        // Hash读写
+        redisTemplate.delete("myHash");
+        redisTemplate.opsForHash().put("myHash", "BJ", "北京");
+        redisTemplate.opsForHash().put("myHash", "SH", "上海");
+        redisTemplate.opsForHash().put("myHash", "HN", "河南");
+        Map<Object, Object> hashCache = redisTemplate.opsForHash().entries("myHash");
+        for (Map.Entry entry : hashCache.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+        System.out.println("---------------");
+    }
+}
